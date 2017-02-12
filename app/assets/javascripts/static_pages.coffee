4e9d680643c5e257ee8a2c153bbd40fa2ce85b94
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-02-12 01:31:16 -0600

    Reset files with material_ui branch.

--- app/assets/javascripts/static_pages.coffee
+++ app/assets/javascripts/static_pages.coffee
@@ -1,26 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-
-$(document).ready ->
-  #form id
-  console.log "new user events binded."
-  $('#new_user').bind('ajax:success', (evt, data, status, xhr) ->
-    #function called on status: 200 (for ex.)
-    console.log 'success'
-    alert = $('#login-alert')
-    alert.text "Success"
-    alert.toggleClass("alert-danger")
-    alert.toggleClass("alert-success")
-    alert.hidden = 'shown'
-    return
-  ).bind 'ajax:error', (evt, xhr, status, error) ->
-    #function called on status: 401 or 500 (for ex.)
-    console.log xhr.responseText
-    console.log error
-    alert = $('#login-alert')
-    alert.text error
-    alert.hidden = 'shown'
-    debugger
-    return
-  return
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,8 @@
 class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
+# skip_before_filter :verify_authenticity_token, :only => :create
+# protect_from_forgery
+  layout 'centered'
 
   # GET /resource/sign_in
   # def new
@@ -8,15 +11,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    resource = User.find_for_database_authentication(email: params[:user][:email])
-    return invalid_login_attempt unless resource
-
-    if resource.valid_password?(params[:user][:password])
-      sign_in :user, resource
-      return render nothing: true
-    end
-
-    invalid_login_attempt
+    super
   end
 
   # DELETE /resource/sign_out
@@ -26,13 +21,8 @@ class Users::SessionsController < Devise::SessionsController
 
   # protected
 
-  # You can put the params you want to permit in the empty array.
+  # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
   # end
-
-  def invalid_login_attempt
-    set_flash_message(:alert, :invalid)
-    render json: flash[:alert], status: 401
-  end
 end
