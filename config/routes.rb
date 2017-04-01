a32397aefab61e9adfbc1e3f3eb44d3c96dbb2e1
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-04-01 12:20:22 -0600

    devise and devise_token_auth routes modifications.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'users', skip: [:omniauth_callbacks]
-    end
-  end
-
-
   devise_for :users, controllers: {
       sessions: 'users/sessions',
       registrations: 'users/registrations'
   }
 
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'users', skip: [:omniauth_callbacks]
+    end
+  end
 
   resources :reports
 
