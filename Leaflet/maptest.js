589620779588da055373138489ceb51fa1cd8095
Luis Alonso Murillo Rojas <luismurirojas@gmail.com>
2017-04-09 16:24:17 -0600

    Remove Leaflet popup to the wildfire markers

--- Leaflet/maptest.js
+++ Leaflet/maptest.js
@@ -10,46 +10,6 @@
 //Global variables
 var currentFireCoordinates = null;
 
-var fireIcon = L.icon({
-     iconUrl: 'fire.png',
-     iconSize:     [32, 37],
-     iconAnchor:   [16, 36],
-     popupAnchor:  [0, -37]
- });
-
- function onEachFeature(feature, layer) {
-     layer.setIcon(fireIcon);
-     if (feature.properties) {
-       var link = '<br><a href="javascript:displayWildfiresDetails('
-                  + feature.properties.LATITUDE + ','
-                  + feature.properties.LONGITUDE + ','
-                  + feature.properties.BRIGHTNESS + ','
-                  + feature.properties.SCAN + ','
-                  + feature.properties.TRACK + ','
-                  + '\'' + feature.properties.ACQ_DATE + '\','
-                  + '\'' + feature.properties.ACQ_TIME + '\','
-                  + '\'' + feature.properties.SATELLITE + '\','
-                  + feature.properties.CONFIDENCE + ','
-                  + '\'' + feature.properties.VERSION + '\','
-                  + feature.properties.BRIGHT_T31 + ','
-                  + feature.properties.FRP + ','
-                  + '\'' + feature.properties.DAYNIGHT + '\''
-                  + ');">Details</a>';
-
-       layer.bindPopup("<b>Wildfire point</b>"
-                      + "<br>BRIGHTNESS: " + feature.properties.BRIGHTNESS
-                      + "<br>DATE: " + feature.properties.ACQ_DATE
-                      + "<br>TIME: " + feature.properties.ACQ_TIME
-                      + "<br>SATELLITE: " + feature.properties.SATELLITE
-                      + link);
-     }
-     layer.on('click', function (e) {
-       map.setView(e.latlng, 13);
-       currentFireCoordinates = L.latLng(e.latlng.lat, e.latlng.lng);
-       removeRoute();
-    });
- }
-
  function displayWildfiresDetails(lat, lng, brightness, scan, track, date, time, satellite, confidence, version, bright_t31, frp, daynight) {
    var jsonMODIS = {"LATITUDE":lat,
                     "LONGITUDE":lng,
@@ -80,13 +40,6 @@ var fireIcon = L.icon({
      console.log("Error trying to invoke mobile method");
    }
  }
-
-/* MODIS geoJSON */
-
- var geojsonLayer = new L.GeoJSON.AJAX("http://forestdev6339.cloudapp.net/Leaflet/central_america.json", {
-    onEachFeature: onEachFeature
-});
-geojsonLayer.addTo(map);
 
 /* Routing */
 
@@ -124,3 +77,65 @@ var fireStationIcon = L.icon({
  function removeFireStationMark() {
    map.removeLayer(fireStationIcon);
  }
+
+ /* Wildfire icon */
+
+ var fireIcon = L.icon({
+      iconUrl: 'fire.png',
+      iconSize:     [32, 37],
+      iconAnchor:   [16, 36],
+      popupAnchor:  [0, -37]
+  });
+
+  function onEachFeature(feature, layer) {
+      layer.setIcon(fireIcon);
+      /*if (feature.properties) {
+        var link = '<br><a href="javascript:displayWildfiresDetails('
+                   + feature.properties.LATITUDE + ','
+                   + feature.properties.LONGITUDE + ','
+                   + feature.properties.BRIGHTNESS + ','
+                   + feature.properties.SCAN + ','
+                   + feature.properties.TRACK + ','
+                   + '\'' + feature.properties.ACQ_DATE + '\','
+                   + '\'' + feature.properties.ACQ_TIME + '\','
+                   + '\'' + feature.properties.SATELLITE + '\','
+                   + feature.properties.CONFIDENCE + ','
+                   + '\'' + feature.properties.VERSION + '\','
+                   + feature.properties.BRIGHT_T31 + ','
+                   + feature.properties.FRP + ','
+                   + '\'' + feature.properties.DAYNIGHT + '\''
+                   + ');">Details</a>';
+
+        layer.bindPopup("<b>Wildfire point</b>"
+                       + "<br>BRIGHTNESS: " + feature.properties.BRIGHTNESS
+                       + "<br>DATE: " + feature.properties.ACQ_DATE
+                       + "<br>TIME: " + feature.properties.ACQ_TIME
+                       + "<br>SATELLITE: " + feature.properties.SATELLITE
+                       + link);
+      }*/
+      layer.on('click', function (e) {
+        //console.log(e);
+        map.setView(e.latlng, 13);
+        currentFireCoordinates = L.latLng(e.latlng.lat, e.latlng.lng);
+        displayWildfiresDetails(e.target.feature.properties.LATITUDE,
+                                e.target.feature.properties.LONGITUDE,
+                                e.target.feature.properties.BRIGHTNESS,
+                                e.target.feature.properties.SCAN,
+                                e.target.feature.properties.TRACK,
+                                e.target.feature.properties.ACQ_DATE,
+                                e.target.feature.properties.ACQ_TIME,
+                                e.target.feature.properties.SATELLITE,
+                                e.target.feature.properties.CONFIDENCE,
+                                e.target.feature.properties.VERSION,
+                                e.target.feature.properties.BRIGHT_T31,
+                                e.target.feature.properties.FRP,
+                                e.target.feature.properties.DAYNIGHT);
+     });
+  }
+
+  /* MODIS geoJSON */
+
+   var geojsonLayer = new L.GeoJSON.AJAX("http://forestdev6339.cloudapp.net/Leaflet/central_america.json", {
+      onEachFeature: onEachFeature
+  });
+  geojsonLayer.addTo(map);
