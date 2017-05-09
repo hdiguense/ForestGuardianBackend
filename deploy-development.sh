3d2ed21edd1909f856d05bc1d498e20191c8c5ad
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-05-08 20:09:32 -0600

    Improve automation and fix json view of forest fires data.

--- deploy-development.sh
+++ deploy-development.sh
@@ -2,17 +2,20 @@
 # Assumes you have a docker container running locally, docker-machine can be used for that. Just execute
 # docker-machine create --driver virtualbox forestguardian-dev;
 
-function fg_activate_dev {
+function fg_activate_machine {
    eval $(docker-machine env forestguardian-dev)
 }
 
 function fg_clean {
+    fg_activate_machine
    docker-compose -f docker-compose-development.yml down
    docker volume rm $(docker volume ls -f dangling=true -q)
 }
 
 function fg_start {
     set -x
+    fg_activate_machine
+    docker-compose -f docker-compose-development.yml build
     docker-compose -f docker-compose-development.yml up -d redis db
     sleep 10
     RAILS_ENV=production docker-compose -f docker-compose-development.yml run web bundle exec rake db:create
@@ -24,11 +27,13 @@ function fg_start {
 }
 
 function fg_reload {
+    fg_activate_machine
     docker-compose -f docker-compose-development.yml stop web
     docker-compose -f docker-compose-development.yml build web
-    docker-compose -f docker-compose-development.yml start web
+    docker-compose -f docker-compose-development.yml up -d web
 }
 
 function fg_ip {
+    fg_activate_machine
     docker-machine ip forestguardian-dev
 }
--- deploy-production.sh
+++ deploy-production.sh
@@ -1,1 +1,37 @@
 #!/usr/bin/env bash
+
+function fg_activate_machine {
+   eval $(docker-machine env forestguardian)
+}
+
+function fg_clean {
+   fg_activate_machine
+   docker-compose -f docker-compose-production.yml down
+   docker volume rm $(docker volume ls -f dangling=true -q)
+}
+
+function fg_start {
+    set -x
+    fg_activate_machine
+    docker-compose -f docker-compose-production.yml build
+    docker-compose -f docker-compose-production.yml up -d redis db
+    sleep 10
+    RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:create
+    RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:schema:load
+    RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:migrate
+    RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:seed
+    docker-compose -f docker-compose-production.yml up -d sidekiq web
+    set +x
+}
+
+function fg_reload {
+    fg_activate_machine
+    docker-compose -f docker-compose-production.yml stop web
+    docker-compose -f docker-compose-production.yml build web
+    docker-compose -f docker-compose-production.yml up -d web
+}
+
+function fg_ip {
+    fg_activate_machine
+    docker-machine ip forestguardian
+}
