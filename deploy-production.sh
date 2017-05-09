92ae5c748977c939a5042f7ce1ab2c73583e0617
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-05-08 22:49:32 -0600

    Add comments.

--- deploy-production.sh
+++ deploy-production.sh
@@ -20,11 +20,14 @@ function fg_start {
     cd ../..
     docker-compose -f docker-compose-production.yml build
     docker-compose -f docker-compose-production.yml up -d redis db
+    # waits for database to be initialized.
     sleep 10
+    # setup database
     RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:create
     RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:schema:load
     RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:migrate
     RAILS_ENV=production docker-compose -f docker-compose-production.yml run web bundle exec rake db:seed
+    # start services that depend on db
     docker-compose -f docker-compose-production.yml up -d sidekiq web
     set +x
 }
