49509c2d1de49941705a1124a141d4e28523d478
Luis Alonso Murillo Rojas <luismurirojas@gmail.com>
2017-05-09 23:07:45 -0600

    Mix the MODIS WMS and preprocessd backedn data

--- Leaflet/fires.js
+++ Leaflet/fires.js
@@ -4,12 +4,31 @@
 
 var map = L.map('map').setView([10.07568504578726, -84.31182861328125], 8);
 
- L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
  	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
  	maxZoom: 18
- }).addTo(map);
+}).addTo(map);
 
- var MODISLayer = L.geoJSON().addTo(map);
+/* Wildfire icon */
+
+var fireIcon = L.icon({
+   iconUrl: 'fire.png',
+   iconSize:     [32, 37],
+   iconAnchor:   [16, 36],
+   popupAnchor:  [0, -37]
+ });
+
+function onEachFeature(feature, layer) {
+  layer.setIcon(fireIcon);
+  /* onClick event */
+  layer.on('click', function (e) {
+    console.log("Fire click!");
+  });
+}
+
+var MODISLayer = L.geoJSON(null, {
+  onEachFeature:onEachFeature
+}).addTo(map);
 
 function downloadMODISData() {
   var bounds = map.getBounds();
@@ -25,6 +44,7 @@ function downloadMODISData() {
 
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
+      console.log(this.responseText);
       var geoJSONData = JSON.parse(this.responseText);
       MODISLayer.clearLayers();
       MODISLayer.addData(geoJSONData);
@@ -38,8 +58,26 @@ function downloadMODISData() {
   xhr.send(data);
 }
 
+function checkZoomLevel() {
+  var zoomLevel = map.getZoom();
+  console.log("Zoom level: " + zoomLevel);
+  if (zoomLevel > 10) {
+    console.log("Download the MODIS data");
+    downloadMODISData();
+  } else {
+    console.log("MODISLayer clear layer");
+    MODISLayer.clearLayers();
+  }
+}
+
 map.on('moveend', function() {
-  downloadMODISData();
+  checkZoomLevel();
 });
 
-downloadMODISData();
+checkZoomLevel();
+
+var wmsLayer = L.tileLayer.wms('https://firms.modaps.eosdis.nasa.gov/wms/c6?', {
+  layers:'fires24',
+  transparent: true,
+  format: 'image/png'
+}).addTo(map);
