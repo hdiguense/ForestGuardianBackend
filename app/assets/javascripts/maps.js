4d7b477f7d0037de34e4a668f5fa23208855045a
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-05-13 21:38:52 -0600

    Merge maps fires.js from @lumurillo with assets.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -16,21 +16,35 @@ var markerIcon;
 var markerArea;
 var reportMarkerLocation = { 'latitude':0.0, 'longitude':0.0 }
 
-function displayWildfiresDetails(lat, lng, brightness, scan, track, date, time, satellite, confidence, version, bright_t31, frp, daynight) {
+/* Interface function with the client app */
+
+//@function displayWildfiresDetails
+//Funtion that parse the MODIS data an send it to the corresponding mobile clients
+//@param {double} lat (latitude) of the fire point
+//@param {double} lng (longitude) of the fire point
+//@param {double} birghtness value of the fire point
+//@param {double} scan value
+//@param {double} track value
+//@param {String} adquisitionTime from the satellite
+//@param {String} satellite identification
+//@param {integer} confidence value
+//@param {String} version of the data
+//@param {double} bright_t31 value
+//@param {double} frp value
+//@param {String} daynight value
+function displayWildfiresDetails(lat, lng, brightness, scan, track, adquisitionTime, satellite, confidence, version, bright_t31, frp, daynight) {
     var jsonMODIS = {"LATITUDE":lat,
         "LONGITUDE":lng,
         "BRIGHTNESS":brightness,
         "SCAN":scan,
         "TRACK":track,
-        "ACQ_DATE":date,
-        "ACQ_TIME":time,
+        "ADQUISITION_TIME":adquisitionTime,
         "SATELLITE":satellite,
         "CONFIDENCE":confidence,
         "VERSION":version,
         "BRIGHT_T31":bright_t31,
         "FRP":frp,
         "DAYNIGHT":daynight};
-    console.log(JSON.stringify(jsonMODIS));
     try {
         mobile.getMODISData(JSON.stringify(jsonMODIS));
     } catch(err) {
@@ -38,6 +52,10 @@ function displayWildfiresDetails(lat, lng, brightness, scan, track, date, time, 
     }
 }
 
+//@function setUserCurrentLocation
+//Funtor that centers the map to the current a given point provided by the clients
+//@param {double} latitued
+//@param {double} longitude
 function setUserCurrentLocation(latitude, longitude) {
 
     // Center map on Location
@@ -54,6 +72,35 @@ function setUserCurrentLocation(latitude, longitude) {
     } catch (err) {
         console.log("Error trying to invoke mobile method");
     }
+}
+
+//@function mobileShowDetails
+//Function that notify the client when is the time to show the detail information of a given fire point
+function mobileShowDetails() {
+    try {
+        mobile.showWildfireDetails();
+    } catch(err) {
+        console.log("Error trying to invoke mobile method");
+    }
+}
+
+//@function setRouteFromTwoPoints
+//Function that sets the route from a point A to a point B
+//@param {double} latitudeA of the point A
+//@param {double} longitudeA of the point A
+//@param {double} latitudeB of the point B
+//@param {double} longitudeB of the point B
+function setRouteFromTwoPoints(latitudeA, longitudeA, latitudeB, longitudeB) {
+    route.setWaypoints([
+        L.latLng(latitudeA, longitudeA),
+        L.latLng(latitudeB, longitudeB)
+    ]);
+}
+
+//@function removeRoute
+//Function that removes the route from the map
+function removeRoute() {
+    route.setWaypoints([]);
 }
 
 function addReportLocation( latitude, longitude ){
@@ -86,17 +133,6 @@ function prepareReportLocation(){
     mobile.reportLocation(reportMarkerLocation.latitude, reportMarkerLocation.longitude);
 }
 
-function setRouteFromTwoPoints(latitudeA, longitudeA, latitudeB, longitudeB) {
-    route.setWaypoints([
-        L.latLng(latitudeA, longitudeA),
-        L.latLng(latitudeB, longitudeB)
-    ]);
-}
-
-function removeRoute() {
-    route.setWaypoints([]);
-}
-
 function addFireStationMark(latitude, longitude) {
     fireStationMarker = L.marker([latitude, longitude], {icon: fireStationIcon});
     fireStationMarker.addTo(map);
@@ -110,19 +146,20 @@ function removeFireStationMark() {
     fireStationMarker.removeFrom(map);
 }
 
-function mobileShowDetails() {
-    try {
-        mobile.showWildfireDetails();
-    } catch(err) {
-        console.log("Error trying to invoke mobile method");
-    }
-}
+/* Pop up functions */
 
+//@function addWildfireMessage
+//Function that creates a popup message with some info related to the pressed fire point
+//@param {double} latitude
+//@param {double} longitude
+//@param {double} brightness value of the wildfire
+//@param {double} temperature value of the place where the fire is located
+//@param {double} humidity value of the place where the fire is located
 function addWildfireMessage(latitude, longitude, brightness, temperature, humidity) {
     var popup = L.popup({offset: L.point(0, -37)})
         .setLatLng(L.latLng(latitude, longitude))
         .setContent('<b>Incendio</b>' +
-            '<br>Intecidad: ' + brightness +
+            '<br>Intensidad: ' + brightness +
             '<br>Temperatura: ' + temperature + " &#8451;" +
             '<br>Humedad: ' + humidity + "%" +
             '<br><a href="javascript:mobileShowDetails();">Detalles</a>')
@@ -133,11 +170,97 @@ function removeWildfireMessage() {
     map.closePopup();
 }
 
+//Callback that will the called each time a fire marker is creator on the map.
+//@param {Object} feature
+//@param {Layer} layer where the marker are been displayed
+function onEachFeature(feature, layer) {
+    layer.setIcon(fireIcon);
+    /* onClick event */
+    layer.on('click', function (e) {
+        displayWildfiresDetails(e.latlng.lat,
+            e.latlng.lng,
+            e.target.feature.properties.brightness,
+            e.target.feature.properties.scan,
+            e.target.feature.properties.track,
+            e.target.feature.properties.acquisition_time,
+            e.target.feature.properties.satellite,
+            e.target.feature.properties.confidence,
+            e.target.feature.properties.version,
+            e.target.feature.properties.bright_t31,
+            e.target.feature.properties.frp,
+            e.target.feature.properties.daynight);
+    });
+}
+//@function downloadMODISData
+//Function that downloads the MODIS data from the backend
+function downloadMODISData() {
+    var bounds = map.getBounds();
+
+    var data = new FormData();
+    data.append("north", bounds.getNorth());
+    data.append("south", bounds.getSouth());
+    data.append("east", bounds.getEast());
+    data.append("west", bounds.getWest());
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            try {
+                var geoJSONData = JSON.parse(this.responseText);
+                MODISLayer.clearLayers();
+                MODISLayer.addData(geoJSONData);
+            } catch (err) {
+                console.log("Error downloading the MODIS data");
+            }
+        }
+    });
+
+    xhr.open("POST", "http://app.forestguardian.org/modis_data/fires.json");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.setRequestHeader("postman-token", "300ba138-f388-a8c1-20c9-57f27367e9ba");
+
+    xhr.send(data);
+}
+
+/* Map events functions */
+
+//@function checkZoomLevel
+//Function that check the the zoom level of the map in order to decide if display or not the datailed MODIS data
+function checkZoomLevel() {
+    var zoomLevel = map.getZoom();
+    console.log("Zoom level: " + zoomLevel);
+    if (zoomLevel > 11) {
+        console.log("Download the MODIS data");
+        downloadMODISData();
+    } else {
+        console.log("MODISLayer clear layer");
+        MODISLayer.clearLayers();
+    }
+}
+
+
 $(function() {
     var attr_osm = 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors',
         attr_overpass = 'POI via <a href="http://www.overpass-api.de/">Overpass API</a>';
     var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {opacity: 0.7, attribution: [attr_osm, attr_overpass].join(', ')});
     map = new L.Map('map').addLayer(osm).setView(L.latLng(10.07568504578726, -84.31182861328125), 8);
+
+    /* MODIS data layers */
+
+    //Data from the backend
+    var MODISLayer = L.geoJSON(null, {
+        onEachFeature:onEachFeature
+    }).addTo(map);
+
+    //NASA's WMS service
+    var wmsLayer = L.tileLayer.wms('https://firms.modaps.eosdis.nasa.gov/wms/c6?', {
+        layers:'fires24',
+        transparent: true,
+        format: 'image/png'
+    }).addTo(map);
+
 
     /* Routing */
     route = L.Routing.control({
@@ -182,32 +305,16 @@ $(function() {
 
     /* MODIS geoJSON */
 
-    function onEachFeature(feature, layer) {
-        layer.setIcon(fireIcon);
-        layer.on('click', function (e) {
-            //console.log(e);
-            map.setView(e.latlng, 13);
-            currentFireCoordinates = L.latLng(e.latlng.lat, e.latlng.lng);
-            displayWildfiresDetails(e.target.feature.properties.LATITUDE,
-                e.target.feature.properties.LONGITUDE,
-                e.target.feature.properties.BRIGHTNESS,
-                e.target.feature.properties.SCAN,
-                e.target.feature.properties.TRACK,
-                e.target.feature.properties.ACQ_DATE,
-                e.target.feature.properties.ACQ_TIME,
-                e.target.feature.properties.SATELLITE,
-                e.target.feature.properties.CONFIDENCE,
-                e.target.feature.properties.VERSION,
-                e.target.feature.properties.BRIGHT_T31,
-                e.target.feature.properties.FRP,
-                e.target.feature.properties.DAYNIGHT);
-        });
-    }
-
     var geojsonLayer = new L.GeoJSON.AJAX("/modis_data/fires.json", {
         onEachFeature: onEachFeature
     });
 
     geojsonLayer.addTo(map);
+
+    //Capturing the moveend event from the map
+    map.on('moveend', function() {
+        checkZoomLevel();
+    });
+    //Initial check action
 
 });
