4a05a5c03a10ee120dab246278809ac9736dd6f9
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-06-09 12:37:37 -0600

    Layers are load in js.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -249,7 +249,6 @@ function checkZoomLevel() {
     }
 }
 
-
 $(function() {
     //Map where the data will be displayed
     map = L.map('map').setView([10.07568504578726, -84.31182861328125], 8);
@@ -320,8 +319,113 @@ $(function() {
         format: 'image/png'
     }).addTo(map);
 
-    for ( var layer in fg_layers ){
-        layer.addTo(map)
-    }
+
+    if ( $('#map').hasClass("weather_perspective") ){
+        /* Central America weather perspectives */
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers: 'perspectiva_ca_mjj',
+            styles: 'perspectiva_ca_mjj',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+    };
+    if ( $('#map').hasClass("forests") ){
+        /* Forest types for Costa Rica */
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi18_tipos_bosque_costa_rica_2015',
+            styles: 'bi18_tipos_bosque_costa_rica_2015',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Forest types for Honduras */
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi21_tipos_bosque_honduras_2015_v2',
+            styles: 'bi21_tipos_bosque_honduras_2015',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Forest types for El Salvador */
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi19_tipos_bosque_el_salvador_2015',
+            styles: 'bi19_tipos_bosque_el_salvador_2015',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Forest types for Belice */
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi15_tipos_bosque_belice_2015',
+            styles: 'bi15_tipos_bosque_belice_2015',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+    };
+    if ( $('#map').hasClass("protected_areas") ){
+        /* Protected areas for Costa Rica */
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi08_areas_prote_costa_rica_2014',
+            styles: 'bi07_areas_prote_belice_2014',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Protected areas for Honduras*/
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi11_areas_prote_honduras_2014',
+            styles: 'bi07_areas_prote_belice_2014',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Protected areas for El Salvador*/
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi09_areas_prote_salvador_2014',
+            styles: 'bi07_areas_prote_belice_2014',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Protected areas for Belice*/
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi07_areas_prote_belice_2014',
+            styles: 'bi07_areas_prote_belice_2014',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Protected areas for Panama*/
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi13_areas_prote_panama_2014',
+            styles: 'bi07_areas_prote_belice_2014',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Protected areas for Guatemala*/
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi10_areas_prote_guatemala_2014',
+            styles: 'bi07_areas_prote_belice_2014',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Protected areas for Caribbean*/
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi01_areas_protegidas_caribe_2008',
+            styles: 'bi01_areas_protegidas_caribe_2008',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+
+        /* Protected areas for Nicaragua*/
+        var wmsLayer = L.tileLayer.wms('http://138.68.63.173/geoserver/ows?', {
+            layers:'bi12_areas_prote_nicaragua_2014',
+            styles: 'bi07_areas_prote_belice_2014',
+            transparent: true,
+            format: 'image/png'
+        }).addTo(map);
+    };
 
 });
